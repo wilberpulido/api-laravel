a6afabe9f396e85d600d50f0079a7ea40b4a49f2
wilberpulido <pulidowilber@gmail.com>
2022-02-05 17:01:12 -0300

    create route for api and testing

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'created_at'
+    ];
+
+    public function getExcerptAttribute()
+    {
+        return substr($this->content,0,120);
+    }
+    public function getPublishedAtAttribute()
+    {
+//        return $this->created_at->format('d/m/Y');
+        return $this->created_at->diffForHumans();
+    }
 }
--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,13 +13,22 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <script src="https://cdn.tailwindcss.com"></script>
     </head>
-    <body>
-        @foreach($posts as $post)
-             <div>
-                <h2>{{ $post->title }}</h2>
-             </div>
-        @endforeach
-        {{ $posts->links() }}
+    <body class="bg-gray-100 text-gray-700">
+    <div class="container mx-auto px-4">
+        <div class="grid grid-cols-3 my-10">
+            @foreach($posts as $post)
+                <div class="bg-white hover:bg-blue-200 border border-gray-200 p-5">
+                    <h2 class="font-bold text-lg mb-4">{{ $post->title }}</h2>
+                    <p class="text-xs"> {{ $post->excerpt }}</p>
+                    <p class="text-xs float-right"> {{ $post->published_at }}</p>
+                </div>
+            @endforeach
+        </div>
+        <div class="mb-10">
+            {{ $posts->links() }}
+        </div>
+    </div>
     </body>
 </html>
